422b6ecb2ad41c9aa0bfecb45e8d35a39fe3be53
j309999174 <j309999174@j309999174demac.local>
2018-12-07 13:48:36 -0800

    Initial Commit

---
+++ appios1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appios1
+//
+//  Created by j309999174 on 2018/12/7.
+//  Copyright © 2018 j309999174. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
